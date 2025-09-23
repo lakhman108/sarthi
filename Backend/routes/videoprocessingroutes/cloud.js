76612eb8bf946f5f13524f35d29edad34c1820b5
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2025-09-24 02:35:03 +0530

    changed from s3 to minio

--- Backend/routes/videoprocessingroutes/cloud.js
+++ Backend/routes/videoprocessingroutes/cloud.js
@@ -6,7 +6,9 @@ const path = require('path');
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION || 'us-east-1'
+  region: process.env.AWS_REGION || 'us-east-1',
+  endpoint: process.env.MINIO_BROWSER_REDIRECT_URL, // your MinIO endpoint
+  s3ForcePathStyle: true // important for MinIO
 });
 
 const BUCKET_NAME = process.env.AWS_S3_BUCKET_NAME;
@@ -94,8 +96,8 @@ const uploadToS3 = async (localFolderPath, s3FolderPath) => {
     }
 
     // Return the S3 URL for the master playlist
-    const masterPlaylistUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${s3FolderPath}/master.m3u8`;
-
+    // const masterPlaylistUrl = `https://${BUCKET_NAME}.s3.amazonaws.com/${s3FolderPath}/master.m3u8`;
+    const masterPlaylistUrl = `${s3.endpoint.href}${BUCKET_NAME}/${s3FolderPath}/master.m3u8`;
     console.log('[S3 UPLOAD] All files uploaded successfully');
     console.log('[S3 UPLOAD] Master playlist URL:', masterPlaylistUrl);
 
